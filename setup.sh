27cded8a8587ddf1a50658ece1904cf0fa1be64e
Saturn Software <saturnsoftwarecenter@gmail.com>
2020-02-11 12:56:36 -0600

    Automatically install RCM if not already

--- setup.sh
+++ setup.sh
@@ -1,1 +1,15 @@
 #!/bin/bash
+
+if [!mkrc -V]
+then
+    echo "RCM not found."
+    if [apt-get -v]
+    then
+        echo "Adding RCM to debian repos."
+        wget -qO - https://apt.thoughtbot.com/thoughtbot.gpg.key | sudo apt-key add -
+        echo "deb https://apt.thoughtbot.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/thoughtbot.list
+        sudo apt-get update
+        echo "Installing..."
+        sudo apt-get -y install rcm
+    fi
+fi
