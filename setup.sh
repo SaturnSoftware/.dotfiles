cda94fdb1b07410b015a1646d5dd79455077653c
Saturn <sasseones@gmail.com>
2020-02-11 15:53:21 -0600

    finalize formatting

--- setup.sh
+++ setup.sh
@@ -1,15 +1,20 @@
-#!/bin/sh
+#!/bin/bash
 
-if [ !mkrc -V ]
+if hash mkrc && [ "$1" != "-f" ];
 then
+    echo "RCM found."
+    else
     echo "RCM not found."
-    if [ apt-get -v ]
+    if hash apt-get;
     then
         echo "Adding RCM to debian repos."
+        {
         wget -qO - https://apt.thoughtbot.com/thoughtbot.gpg.key | sudo apt-key add -
         echo "deb https://apt.thoughtbot.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/thoughtbot.list
-        sudo apt-get update
+        } &> /dev/null
+        echo "Updating Package lists..."
+        sudo apt-get -qq update
         echo "Installing..."
-        sudo apt-get -y install rcm
+        sudo apt-get -qqy install rcm
     fi
 fi
